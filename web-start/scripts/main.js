e8a5f069cc96ff0fc9164aa8b53c7b7bb62b81c1
Nicolas Garnier <nivco.las@gmail.com>
2016-05-19 12:09:54 -0700

    Updated error message for missing storageBucket attribute

--- web-start/scripts/main.js
+++ web-start/scripts/main.js
@@ -228,8 +228,10 @@ FriendlyChat.prototype.checkSetup = function() {
     window.alert('You have not configured and imported the Firebase SDK. ' +
         'Make sure you go through the codelab setup instructions.');
   } else if (config.storageBucket === '') {
-    window.alert('You have not enabled Firebase Storage prior to importing the firebase SDK. ' +
-        'Make sure you go through the codelab setup instructions.');
+    window.alert('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' +
+        'actually a Firebase bug that occurs rarely.' +
+        'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
+        'and make sure the storageBucket attribute is not empty.');
   }
 };
 
