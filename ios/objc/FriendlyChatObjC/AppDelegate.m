88c65de43a94330ebc24d01a92aa3c4366e9a239
Ibrahim Ulukaya <ulukaya@google.com>
2017-02-10 14:17:48 -0800

    Add FCM to iOS codelabs
    Change-Id: I0c496514f84952b1f48c283e2717bb288f0c4095

--- ios/objc/FriendlyChatObjC/AppDelegate.m
+++ ios/objc/FriendlyChatObjC/AppDelegate.m
@@ -18,15 +18,24 @@
 
 @import Firebase;
 
-@implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application
-didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [FIRApp configure];
-  [GIDSignIn sharedInstance].clientID = [FIRApp defaultApp].options.clientID;
-  [GIDSignIn sharedInstance].delegate = self;
-  return YES;
-}
+//////////////////////////////////////////////////////////////////////////////////
+//                                                                              //
+//       UserNotifications are only required for the optional FCM step          //
+//                                                                              //
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0  //
+@import UserNotifications;                                                      //
+#endif                                                                          //
+// Implement UNUserNotificationCenterDelegate to receive display notification   //
+// via APNS for devices running iOS 10 and above.                               //
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0  //
+@interface AppDelegate () <UNUserNotificationCenterDelegate>                    //
+@end                                                                            //
+#endif                                                                          //
+//////////////////////////////////////////////////////////////////////////////////
+
+
+@implementation AppDelegate
 
 - (BOOL)application:(nonnull UIApplication *)application
             openURL:(nonnull NSURL *)url
@@ -61,6 +70,105 @@ didSignInForUser:(GIDGoogleUser *)user
     }];
   } else {
     NSLog(@"Error %@", error.localizedDescription);
+  }
+}
+
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [FIRApp configure];
+  [GIDSignIn sharedInstance].clientID = [FIRApp defaultApp].options.clientID;
+  [GIDSignIn sharedInstance].delegate = self;
+
+
+////////////////////////////////////////////////////////////////////////
+//                                                                    //
+//  CODE BELOW THIS POINT IS ONLY REQUIRED FOR THE OPTIONAL FCM STEP  //
+//                                                                    //
+////////////////////////////////////////////////////////////////////////
+
+
+  if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_9_x_Max) {
+    UIUserNotificationType allNotificationTypes =
+    (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+    UIUserNotificationSettings *settings =
+    [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+  } else {
+    // iOS 10 or later
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+    UNAuthorizationOptions authOptions =
+    UNAuthorizationOptionAlert
+    | UNAuthorizationOptionSound
+    | UNAuthorizationOptionBadge;
+    [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    }];
+
+    // For iOS 10 display notification (sent via APNS)
+    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+#endif
+  }
+
+  [[UIApplication sharedApplication] registerForRemoteNotifications];
+
+  return YES;
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+  // If you are receiving a notification message while your app is in the background,
+  // this callback will not be fired till the user taps on the notification launching the application.
+  [self showAlert:userInfo];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+  // If you are receiving a notification message while your app is in the background,
+  // this callback will not be fired till the user taps on the notification launching the application.
+  [self showAlert:userInfo];
+
+  completionHandler(UIBackgroundFetchResultNewData);
+}
+
+// Receive displayed notifications for iOS 10 devices.
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+// Handle incoming notification messages while app is in the foreground.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+  // Print message ID.
+  NSDictionary *userInfo = notification.request.content.userInfo;
+  [self showAlert:userInfo];
+
+  // Change this to your preferred presentation option
+  completionHandler(UNNotificationPresentationOptionNone);
+}
+
+// Handle notification messages after display notification is tapped by the user.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void (^)())completionHandler {
+  NSDictionary *userInfo = response.notification.request.content.userInfo;
+  [self showAlert:userInfo];
+
+  completionHandler();
+}
+#endif
+
+- (void)showAlert:(NSDictionary *)userInfo {
+  NSString *apsKey = @"aps";
+  NSString *gcmMessage = @"alert";
+  NSString *gcmLabel = @"google.c.a.c_l";
+
+  NSDictionary *aps = userInfo[apsKey];
+  if (aps) {
+    NSString *message = aps[gcmMessage];
+    if (message) {
+      dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:userInfo[gcmLabel] message:message preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *dismissAction = [UIAlertAction actionWithTitle:@"Dismiss" style:UIAlertActionStyleDestructive handler:nil];
+        [alert addAction:dismissAction];
+        [_window.rootViewController.presentedViewController presentViewController:alert animated: true completion: nil];
+      });
+    }
   }
 }
 
