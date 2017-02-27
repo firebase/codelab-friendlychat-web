32a6e84172b9d2f173e0072db8b9031cee17bdd9
Ibrahim Ulukaya <ulukaya@google.com>
2017-02-26 23:08:51 -0700

    Delete automatic sign in from starter project
    Change-Id: If15b0b5150068cebe21f6d4d0cf70268b3819db7

--- ios-starter/swift-starter/FriendlyChatSwift/SignInViewController.swift
+++ ios-starter/swift-starter/FriendlyChatSwift/SignInViewController.swift
@@ -27,7 +27,6 @@ class SignInViewController: UIViewController, GIDSignInUIDelegate {
   override func viewDidLoad() {
     super.viewDidLoad()
     GIDSignIn.sharedInstance().uiDelegate = self
-    GIDSignIn.sharedInstance().signInSilently()
   }
 
   deinit {
