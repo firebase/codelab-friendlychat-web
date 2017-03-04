49877687e111b503bd92d2104c214b959e4d75fa
Theresa-Megha Bangalore <MeghaB@users.noreply.github.com>
2017-03-03 18:05:00 -0800

    Update MainActivity.java
    Adding the Request id for REQUEST_IMAGE, used in step 8, sending image messages.

--- android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
+++ android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
@@ -92,6 +92,7 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = "MainActivity";
     public static final String MESSAGES_CHILD = "messages";
     private static final int REQUEST_INVITE = 1;
+    private static final int REQUEST_IMAGE = 2;
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 10;
     public static final String ANONYMOUS = "anonymous";
     private static final String MESSAGE_SENT_EVENT = "message_sent";
