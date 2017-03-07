89628f2f176681f72ea4ea5cf00ff6b7dd724a31
Arthur Thompson <arthurthompson@google.com>
2017-03-07 12:58:52 -0800

    Add messageImageView to android starter
    bug: 35960621
    Change-Id: Ifd4ffd693c5663873d8d4ec30950ed3e34d29521

--- android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
+++ android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
@@ -78,12 +78,14 @@ public class MainActivity extends AppCompatActivity
 
     public static class MessageViewHolder extends RecyclerView.ViewHolder {
         public TextView messageTextView;
+        public ImageView messageImageView;
         public TextView messengerTextView;
         public CircleImageView messengerImageView;
 
         public MessageViewHolder(View v) {
             super(v);
             messageTextView = (TextView) itemView.findViewById(R.id.messageTextView);
+            messageImageView = (ImageView) itemView.findViewById(R.id.messageImageView);
             messengerTextView = (TextView) itemView.findViewById(R.id.messengerTextView);
             messengerImageView = (CircleImageView) itemView.findViewById(R.id.messengerImageView);
         }
