1e9295cc26af6194976d3d1c9370f45892ad10a8
Bret McGowen <bretm@google.com>
2017-04-06 12:11:17 -0600

    Deploy firebase-messaging-sw.js to dist

--- cloud-functions-angular-start/src/firebase-messaging-sw.js
+++ cloud-functions-angular-start/src/firebase-messaging-sw.js
@@ -4,6 +4,6 @@ importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  messagingSenderId: '670781077200'
+  messagingSenderId: ''
 });
 var messaging = firebase.messaging();
