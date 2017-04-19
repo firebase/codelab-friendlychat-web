252c9c42d64f526ae86b9ce1757679d66e1b7b2b
Nicolas Garnier <nivco@google.com>
2017-04-19 17:42:53 +0200

    Use auto init in the FCM Service worker.
    Change-Id: I8df975748cc65f9f59ee18d610e29455d1aaecac

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,9 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-messaging.js');
+importScripts('/__/firebase/3.8.0/firebase-app.js');
+importScripts('/__/firebase/3.8.0/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
 
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
-firebase.initializeApp({
-  messagingSenderId: '670781077200'
-});
-var messaging = firebase.messaging();
+firebase.messaging();
