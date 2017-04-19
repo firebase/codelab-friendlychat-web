e8c5545fbe45fa4d278ea70413952baa5b0dead7
Nicolas Garnier <nivco@google.com>
2017-04-19 15:49:31 +0200

    Switch to using implicit initialization
    Change-Id: I849ff17207d956cda03569df314e9664f72a184b

--- web-start/scripts/main.js
+++ web-start/scripts/main.js
@@ -239,16 +239,10 @@ FriendlyChat.prototype.toggleButton = function() {
 
 // Checks that the Firebase SDK has been correctly setup and configured.
 FriendlyChat.prototype.checkSetup = function() {
-  if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
+  if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
     window.alert('You have not configured and imported the Firebase SDK. ' +
-        'Make sure you go through the codelab setup instructions.');
-  } else if (config.storageBucket === '') {
-    window.alert('Your Cloud Storage bucket has not been enabled. Sorry about that. This is ' +
-        'actually a Firebase bug that occurs rarely. ' +
-        'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
-        'and make sure the storageBucket attribute is not empty. ' +
-        'You may also need to visit the Storage tab and paste the name of your bucket which is ' +
-        'displayed there.');
+        'Make sure you go through the codelab setup instructions and make ' +
+        'sure you are running the codelab using `firebase serve`');
   }
 };
 
