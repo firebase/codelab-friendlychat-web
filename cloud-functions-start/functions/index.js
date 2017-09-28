278fb1c992af715f6cd799647a9dd04be5486fcb
faisalahmedansari20 <faisalahmedansari20@gmail.com>
2017-09-28 11:56:35 +0530

    index.js
    test

--- cloud-functions-start/functions/index.js
+++ cloud-functions-start/functions/index.js
@@ -15,8 +15,25 @@
  */
 
 // TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+const functions = require('firebase-functions');
+// Import and initialize the Firebase Admin SDK.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+exports.addWelcomeMessages = functions.auth.user().onCreate(event => {
+  const user = event.data;
+  console.log('A new user signed in for the first time.');
+  const fullName = user.displayName || 'Anonymous';
+
+  // Saves the new welcome message into the database
+  // which then displays it in the FriendlyChat clients.
+  return admin.database().ref('messages').push({
+    name: 'Firebase Bot',
+    photoUrl: '/images/firebase-logo.png', // Firebase logo
+    text: `${fullName} signed in for the first time! Welcome!`
+  });
+});
 
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
 
