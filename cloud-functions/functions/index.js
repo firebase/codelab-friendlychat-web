04dfd1d034eef8bf1f7f1077702ef643f1b4b0de
Nicolas Garnier <nivco@google.com>
2018-04-16 10:01:00 +0200

    fixes #271
    Change-Id: If1d607008e0ab9f990844fc148b315083caf95da

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -101,7 +101,7 @@ exports.sendNotifications = functions.database.ref('/messages/{messageId}').onCr
       title: `${snapshot.val().name} posted ${text ? 'a message' : 'an image'}`,
       body: text ? (text.length <= 100 ? text : text.substring(0, 97) + '...') : '',
       icon: snapshot.val().photoUrl || '/images/profile_placeholder.png',
-      click_action: `https://${functions.config().firebase.authDomain}`,
+      click_action: `https://${process.env.GCLOUD_PROJECT}.firebaseapp.com`,
     }
   };
 
