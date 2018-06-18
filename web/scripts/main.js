e2632723c0eca0f444182820e400575fcada1b3a
Nicolas Garnier <nivco.las@gmail.com>
2018-06-18 22:25:19 +0200

    Using addeventlistener.

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -320,6 +320,6 @@ FriendlyChat.prototype.checkSetup = function() {
   }
 };
 
-window.onload = function() {
+window.addEventListener('load' , function() {
   window.friendlyChat = new FriendlyChat();
-};
+});
