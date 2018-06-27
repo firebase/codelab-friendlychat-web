5fb6652abd9f74ae44e76c20aef992276e17a21f
Nicolas Garnier <nivco@google.com>
2018-06-27 12:51:02 +0200

    Changed comments for consistency

--- web-start/scripts/main.js
+++ web-start/scripts/main.js
@@ -154,7 +154,7 @@ FriendlyChat.prototype.onAuthStateChanged = function(user) {
 
 // Returns true if user is signed-in. Otherwise false and displays a message.
 FriendlyChat.prototype.checkSignedInWithMessage = function() {
-  /* TODO(DEVELOPER): Check if user is signed-in Firebase. */
+  // TODO(DEVELOPER): Check if user is signed-in Firebase.
 
   // Display a message to the user using a Toast.
   var data = {
