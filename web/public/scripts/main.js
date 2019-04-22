d2ff235c6fc1271a90eef882f2da6d6e2c8f144f
Jeff <jhuleatt@google.com>
2019-04-22 11:29:33 -0700

    remove Firestore settings (#407)
    `timestampsInSnapshots: true` is the default now.

--- web/public/scripts/main.js
+++ web/public/scripts/main.js
@@ -354,8 +354,6 @@ initFirebaseAuth();
 
 // Remove the warning about timstamps change. 
 var firestore = firebase.firestore();
-var settings = {timestampsInSnapshots: true};
-firestore.settings(settings);
 
 // We load currently existing chat messages and listen to new ones.
 loadMessages();
