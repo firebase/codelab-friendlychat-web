5e47593d2b1dcb1ac94d2689d3790922bc5aab61
James Liu <jamxl@google.com>
2020-01-25 19:00:29 -0800

    Fix toMillis() called twice on firestore doc message timestamp.

--- performance-monitoring/public/scripts/main.js
+++ performance-monitoring/public/scripts/main.js
@@ -74,7 +74,7 @@ function loadMessages() {
         deleteMessage(change.doc.id);
       } else {
         var message = change.doc.data();
-        displayMessage(change.doc.id, message.timestamp.toMillis(), message.name,
+        displayMessage(change.doc.id, message.timestamp, message.name,
                        message.text, message.profilePicUrl, message.imageUrl);
       }
     });
