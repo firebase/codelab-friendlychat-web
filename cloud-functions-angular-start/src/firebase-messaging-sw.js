d318f077bbe40c3bbcb32fc74a0c64b970593014
DPEBot <dpebot@google.com>
2021-04-30 02:56:43 -0700

    Auto-update dependencies. (#621)

--- cloud-functions-angular-start/src/firebase-messaging-sw.js
+++ cloud-functions-angular-start/src/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/8.4.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.4.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.4.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.4.3/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
