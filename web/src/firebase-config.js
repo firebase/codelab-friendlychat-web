d2403dfa008c88168d553b2e408e1f78aabb67b2
jumpmanzplanz <79822120+jumpmanzplanz@users.noreply.github.com>
2021-10-17 22:33:52 -0400

    Update firebase-config.js

--- web/src/firebase-config.js
+++ web/src/firebase-config.js
@@ -5,8 +5,14 @@
  * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
- */
-const config = {
+ */const firebaseConfig = {
+  apiKey: "AIzaSyBSJW9tIwB_8dq_4GTGdV4XNiFycjT8EWI",
+  authDomain: "friendlychat-e77d8.firebaseapp.com",
+  projectId: "friendlychat-e77d8",
+  storageBucket: "friendlychat-e77d8.appspot.com",
+  messagingSenderId: "964967125202",
+  appId: "1:964967125202:web:3a5b29727ad52584f15d0b"
+};const config = {
   /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
 };
 
