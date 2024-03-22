0e67869bd52d4ab99fcd0f1702347aa8f5c29eaf
marktechson <2554588+MarkTechson@users.noreply.github.com>
2024-03-22 13:17:07 -0500

    update repo to use modern Angular. Note this example doesn't leverage emulators yet.

--- angularfire-start/src/app/app.config.ts
+++ angularfire-start/src/app/app.config.ts
@@ -1,12 +1,33 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator } from '@angular/fire/functions';
+import {
+  provideFirestore,
+  getFirestore,
+  connectFirestoreEmulator,
+} from '@angular/fire/firestore';
+import {
+  provideFunctions,
+  getFunctions,
+  connectFunctionsEmulator,
+} from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import {
+  provideStorage,
+  getStorage,
+  connectStorageEmulator,
+} from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-    providers: [],
-}
+  providers: [
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore()),
+      provideAuth(() => getAuth()),
+      provideFunctions(() => getFunctions()),
+      provideStorage(() => getStorage()),
+      provideMessaging(() => getMessaging())
+    ),
+  ],
+};
